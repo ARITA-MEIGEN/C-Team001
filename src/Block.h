78dfe0afeb219a0391e560445e4d6e599f10ae56
ARITAMEIGEN <211105@st.yoshida-g.ac.jp>
2023-04-17 09:39:11 +0900

    BlockファイルとMapファイルの作成 中身はこれから

---
+++ src/Block.h
@@ -0,0 +1,34 @@
+//=============================================================================
+//
+// ブロック生成
+// Author:arita meigen
+//
+//=============================================================================
+#ifndef _BLOCK_H_			// このマクロ定義がされてなかったら
+#define _BLOCK_H_			// 二重インクルード防止のマクロ定義
+
+//-----------------------------------------------------------------------------
+// include
+//-----------------------------------------------------------------------------
+#include"ObjectX.h"
+#include"Application.h"
+
+//=============================================================================
+// 構造体定義
+//=============================================================================
+// 頂点データ
+class CBlock : public CObjectX
+{
+public:
+	explicit CBlock(int nPriority = 2);
+	~CBlock()override;
+	HRESULT Init()override;
+	void Uninit()override;
+	void Update()override;
+	void Draw()override;
+	static CBlock *Create(D3DXVECTOR3 pos, D3DXVECTOR3 siz, float lot);
+private:
+	//メンバ関数
+};
+
+#endif
