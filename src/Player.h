ec99e6c5f8ed39a052f2459b180ed634cf63a9b7
TakanoKeisuke <211073@st.yoshida-g.ac.jp>
2023-05-26 11:34:48 +0900

    修正

--- src/Player.h
+++ src/Player.h
@@ -52,14 +52,6 @@ public:
 		PST_PAINT,	// 塗り強化状態
 		PST_MAX
 	};
-	
-	// スキル
-	enum PLAYER_SKILL
-	{
-		SKILL_SPEED = PST_SPEED,	// 加速
-		SKILL_PAINT = PST_PAINT,	// 塗り強化
-		SKILL_MAX
-	};
 
 	enum ITEM_STATE
 	{
@@ -83,14 +75,15 @@ public:
 	void Updatepos();			// 座標の更新
 	void Normalization();		// 正規化
 	void BlockCollision();		// ブロックとの判定
+	void Skill();				// スキル処理
 
-	// Setter
+								// Setter
 	void SetController(CController* inOperate);
 	void SetPos(D3DXVECTOR3 pos) { m_pos = pos; };			// 位置の設定
 	void SetRot(D3DXVECTOR3 rot) { m_rot = rot; };			// 向きの設定
 	void SetSkillGauge(int skill) { m_nSkillGauge = skill; }	// スキルゲージの量の設定
 
-	// Getter
+																// Getter
 	D3DXVECTOR3		GetPos() { return m_pos; };
 	PLAYER_MOTION	GetNowMotion() { return m_Motion; };
 	D3DXMATRIX		GetMtx() { return m_mtxWorld; };				//マトリックスの取得
@@ -98,13 +91,9 @@ public:
 	int				GetPlayerNumber() { return m_nPlayerNumber; }	//プレイヤーの番号の取得
 
 private:
-	void SkillPaint();
-	void Skill();					// スキル処理
 	void TurnLookAtMoveing();		// 移動方向を見て曲がる
 	void StopNoBlock();				// ブロックがない場所で停まる
-	void MoveSwitchAtCenterBlock();	// ブロックの中心で移動を切り替える
-	void TakeItem();				// アイテムを拾う
-	void KnockBack(const D3DXVECTOR3& move,const float power);				// ノックバック
+	void TurnCenterBlock();				// ブロックがない場所で停まる
 private:
 	CController*	m_controller;					// 命令を出す人
 	CObjectX*		m_apModel[NUM_PLAYERPARTS];		// モデルのインスタンス
@@ -114,7 +103,7 @@ private:
 	D3DXVECTOR3		m_rot;							// 向き
 	D3DXVECTOR3		m_move;							// 移動量
 	D3DXVECTOR3		m_moveVec;						// 移動ベクトル
-	D3DXVECTOR3		m_movePlanVec;					// 移動予定ベクトル
+	D3DXVECTOR3		m_movePlanVec;						// 移動予定ベクトル
 	D3DXVECTOR3		m_posold;						// 前回の位置
 	D3DXVECTOR3		m_rotDest;						// 目的の角度の保存
 	PLAYER_MOTION	m_Motion;						// 現在のモーション
@@ -129,12 +118,7 @@ private:
 	CShadow*		m_pShadow;						// 影
 	CBlock*			m_pOnBlock;						// プレイヤーの乗っているブロックへのポインタ
 
-	// ノックバック関係
-	bool m_isKnockBack;				// ノックバック状態か否か
-	unsigned int m_knockBackTime;	// ノックバック時間
-	unsigned int m_knockBackCnt;	// ノックバック経過時間
-
-	//押し出し判定関連
+													//押し出し判定関連
 	D3DXVECTOR3		m_aAxisSiz[PST_MAX];			// 押し出し判定の大きさ
 };
 
