3578fdf2b1279b7d49e6e1ae80b469001e446b2a
ARITAMEIGEN <211105@st.yoshida-g.ac.jp>
2023-09-27 12:13:06 +0900

    カメラの描画範囲とリザルトの空の位置を調整

--- src/sky_bg.cpp
+++ src/sky_bg.cpp
@@ -12,8 +12,8 @@ HRESULT CSkyBg::Init()
 {
 	CObject3D::Init();
 	m_uPos = 0.0f;
-	SetPos(D3DXVECTOR3(0.0f, 500.0f, 2700.0f));
-	SetSiz(D3DXVECTOR3(6000.0f, 0.0f, 1500.0f));
+	SetPos(D3DXVECTOR3(200.0f, 500.0f, 1700.0f));
+	SetSiz(D3DXVECTOR3(6000.0f, 0.0f, 8500.0f));
 	SetRot(D3DXVECTOR3(-1.57f, 0.0f, 0.0f));
 	SetTextureKey("SKY");
 	return S_OK;
